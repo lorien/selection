e703fa086eaf15feb95a7bb728e1e6cf2e4cf20b
Gregory Petukhov <lorien@lorien.name>
2015-02-17 04:50:54 +0700

    Bump version: 0.0.4 → 0.0.5

--- selection/__init__.py
+++ selection/__init__.py
@@ -2,4 +2,4 @@ from selection.selector_list import SelectorList, RexResultList  # noqa
 from tools.const import NULL  # noqa
 from selection.backend.xpath import XpathSelector  # noqa
 
-version = '0.0.4'
+version = '0.0.5'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'selection',
-    version = '0.0.4',
+    version = '0.0.5',
     description = 'API to extract content from HTML & XML documents',
     author = 'Gregory Petukhov',
     author_email = 'lorien@lorien.name',
