0660d46e638b76933f509cae30194e0056fc9038
Gregory Petukhov <lorien@lorien.name>
2015-08-14 16:02:44 +0600

    Bump version: 0.0.9 → 0.0.10

--- selection/__init__.py
+++ selection/__init__.py
@@ -2,4 +2,4 @@ from selection.selector_list import SelectorList, RexResultList  # noqa
 from weblib.const import NULL  # noqa
 from selection.backend.xpath import XpathSelector  # noqa
 
-version = '0.0.9'
+version = '0.0.10'
