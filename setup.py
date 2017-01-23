24ce556d9d0261093dfd504c5eeca03e9a3790b8
Gregory Petukhov <lorien@lorien.name>
2017-01-23 23:28:48 +0700

    Bump version: 0.0.12 → 0.0.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'selection',
-    version = '0.0.12',
+    version = '0.0.13',
     description = 'API to extract content from HTML & XML documents',
     author = 'Gregory Petukhov',
     author_email = 'lorien@lorien.name',
