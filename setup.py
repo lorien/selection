866c7ee06ed3f00995af71886dab432cb04366cc
User <guest@example.com>
2025-09-14 01:50:10 +0300

    Add typing-extension as deps for py2. Configure universal wheel builds.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         'lxml;platform_system!="Windows"',
         "six",
+        'typing-extensions; python_version <= "2.7"',
     ],
     extras_require={
         "pyquery": [
