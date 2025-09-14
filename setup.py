e55d2c364c31239025b66c4d05b9734e3c6ffca0
User <guest@example.com>
2025-09-14 13:07:39 +0300

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="selection",
-    version="2.0.0",
+    version="2.0.1",
     packages=["selection"],
     install_requires=[
         'lxml;platform_system!="Windows"',
